dd49fc6f119f8971556fd5f57d34909fac7bded8
Matheus Khatib <khatibprojetos@gmail.com>
2021-09-30 13:15:41 -0300

    adicionei selection sort

---
+++ selection_sort.py
@@ -0,0 +1,15 @@
+﻿def ordena(lista):
+    for i in range(len(lista)):
+        num_minimo = lista[i]
+        for j in range(i+1, len(lista)):
+            if lista[j]<num_minimo:
+                num_minimo=lista[j]
+        ind = lista.index(num_minimo)
+        lista[i],lista[ind]=lista[ind],lista[i]
+    return lista
+ 
+
+   
+  
+   
+        
